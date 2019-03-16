65f233d5643c7f3b285b253ea54c7d5da63d6a24
Sam Wong <sam_wong156@hotmail.com>
2019-03-16 18:57:20 -0400

    mutt

---
+++ BashScript/dock.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --auto --rotate normal --output DP1 --off --output DP2-1 --auto --above eDP1 --rotate normal --output DP2-2 --off --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+feh --bg-fill /home/sam/Images/Backgrounds/Fire.jpg --bg-fill /home/sam/Images/Backgrounds/HillCity.png
